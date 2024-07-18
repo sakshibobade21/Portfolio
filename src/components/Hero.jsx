e14b89dbb1006679b20bb365b644ca2f40003c7a
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-07-18 15:13:06 +0530

    resume button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import '../styles/HeroStyles.css';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import Button from '@mui/material/Button';
 
 const Hero = () => {
+
   return (
-    <div>
+    <div id="intro-section">
       <div id="intro-header">
         <div id="veticle-line"></div>
-        <div id="hero-header">
-          <h1>Hi, I'm Sakshi</h1>
-          <h3>Full Stack Developer, Crafting Dynamic Web Applications & User-Centric Interfaces</h3>
+        <div id='about-me'>
+          <div id="hero-header">
+            <h1>Hi, I'm Sakshi</h1>
+            <h3>Full Stack Developer, Crafting Dynamic Web Applications & User-Centric Interfaces</h3>
+          </div>
+          <div id='about-icons'>
+            <div id="icon" className='linkedin-icon'><LinkedInIcon /></div>
+            <div id="icon" className='github-icon'><GitHubIcon /></div>
+            <div id="icon" className='email-icon'><EmailIcon /></div>
+            <div id="icon" className='phone-icon'><PhoneIcon /></div>
+          </div>
+          <div><Button id="resume-button" variant="outlined" size="medium"><span id='text-resume'>RESUME</span></Button>
+          </div>
         </div>
         <div id="image-div"></div>
       </div>
