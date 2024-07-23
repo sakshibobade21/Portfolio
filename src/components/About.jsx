bd0eec4eab1a3a6c3fa62229e7a8d385185e6bd4
Sakshi Bobade <bobadesakshi21@gmail.com>
2024-07-23 13:44:48 +0530

    Adding the skill section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import '../styles/About.css'
+import React from 'react';
+import '../styles/About.css';
+import { ABOUT_ME, SKILLS, SKILLS_IMG } from '../utils/constant';
+
 
 const About = () => {
   return (
     <div id='about-div'>
       <h1 id='overview-heading'>Overview</h1>
+      <div id="desc-header">A Bit About Me </div>
+      <div id="description">{ABOUT_ME}</div>
+      <div id="technologies">Technical Palette</div>
+      <div id="skill-set">
+        {SKILLS.map((skill,index) => (
+          <div id="skill-item" key={index}>
+           <img src={SKILLS_IMG[index]}/>
+           <div id='skill-name'>{skill}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,38 @@
+import jsImg from '../assets/tech/javascript.png';
+import htmlImg from '../assets/tech/html.png';
+
+export const ABOUT_ME = "I am a Software Engineer ||| with a passion for developing seamless user interfaces and backend systems. My experience includes working on complex projects, solving challenging problems, and continuously learning new technologies to stay ahead in the fast-evolving tech landscape. I thrive in dynamic environments where I can contribute to impactful products";
+
+export const SKILLS = [ 
+  "Javascript",
+  "Node JS", 
+  "Express", 
+  "React", 
+  "Redux",
+  "Typescript",
+  "MySQL",
+  "Mongo DB",
+  "GraphQL",
+  "Web Sockets",
+  "Redis",
+  "ElectronJS",
+  "Java",
+  "Spring Boot",
+  "Bootstrap",
+  "JWT",
+  "Parcel",
+  "Docker",
+  "Kuberetes",
+  "Jenkins",
+  "HTML",
+  "CSS",
+  "Tailwind CSS",
+  "Angular",
+  "Postman",
+  "Git",
+  "Jest",
+  "Mocha",
+  "Chai"
+ ];
+
+export const SKILLS_IMG = [ jsImg, htmlImg ];
